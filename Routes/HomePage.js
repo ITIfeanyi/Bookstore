2ed63e67ea67205be5ff9eb1b8c63535e58c565a
ITIfeanyi <ifeanyithankgodigweh@gmail.com>
2021-03-30 00:20:13 -0700

    added try catch block

--- Routes/HomePage.js
+++ Routes/HomePage.js
@@ -5,13 +5,17 @@ const User = require("../models/UserSchema");
 const Book = require("../models/bookSchema");
 
 router.get("/", async (req, res) => {
-  const books = await Book.find({});
-  res.locals.books = books;
+  try {
+    const books = await Book.find({});
+    res.locals.books = books;
 
-  res.render("homepage", {
-    title: "Home | Book-Africa",
-    books,
-  });
+    res.render("homepage", {
+      title: "Home | Book-Africa",
+      books,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
--- Routes/signleBook.js
+++ Routes/signleBook.js
@@ -1,30 +1,32 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/UserSchema");
-
 const Book = require("../models/bookSchema");
 
 router.get("/book/:ID", async (req, res) => {
-  const id = req.params.ID;
-  const books = await Book.find({});
-  const singleBook = await Book.findById(id);
-  res.locals.singleBook = singleBook;
-  res.locals.books = books;
+  try {
+    const id = req.params.ID;
+    const books = await Book.find({});
+    const singleBook = await Book.findById(id);
+    res.locals.singleBook = singleBook;
+    res.locals.books = books;
 
-  const bookSize = (() => {
-    if (singleBook.FileSize >= 1048576) {
-      return (singleBook.FileSize / 1048576).toFixed(2) + "MB";
-    }
-    if (singleBook.FileSize >= 1024) {
-      return (singleBook.FileSize / 1048576).toFixed(0) + "KB";
-    }
-    return null;
-  })();
-  res.locals.bookSize = bookSize;
-  res.render("singleBook", {
-    title: singleBook.BookTitle,
-    singleBook,
-    bookSize,
-  });
+    const bookSize = (() => {
+      if (singleBook.FileSize >= 1048576) {
+        return (singleBook.FileSize / 1048576).toFixed(2) + "MB";
+      }
+      if (singleBook.FileSize >= 1024) {
+        return (singleBook.FileSize / 1048576).toFixed(0) + "KB";
+      }
+      return null;
+    })();
+    res.locals.bookSize = bookSize;
+    res.render("singleBook", {
+      title: singleBook.BookTitle,
+      singleBook,
+      bookSize,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 module.exports = router;
